f7e6ea1a28fed4e0612d69e434619adffd990d8d
Piter N. <gabrielliendo1257@gmail.com>
2024-12-26 11:00:26 -0500

    .

--- item/src/main/java/com/guille/springcloud/microservicio/item/client/ProductFeignClient.java
+++ item/src/main/java/com/guille/springcloud/microservicio/item/client/ProductFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "127.0.0.1:8081/api/v1/product", name = "product")
+@FeignClient(name = "product")
 public interface ProductFeignClient {
 
   @GetMapping(value = "/all") List<Product> getFullProducts();
